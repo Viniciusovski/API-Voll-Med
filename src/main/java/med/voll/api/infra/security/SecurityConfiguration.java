9fd2eee898b685d334273020ca5fbfd37f50a323
Viniciusovski <vini98.2012@gmail.com>
2023-07-15 19:05:45 -0300

    Autenticando o usuario

--- src/main/java/med/voll/api/infra/security/SecurityConfiguration.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfiguration.java
@@ -1,7 +1,9 @@
 package med.voll.api.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,18 +12,28 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity // Habilita a manipulação das configurações de segurança
 public class SecurityConfiguration {
 	
+	@Autowired
+	private SecurityFilter securityFilter;
+	
 	// O objectivo é fazer a aplicação deixar de ser statefull para ser stateless
 	// Desabilita o processo padrão do spring para segurança
-	@Bean 
+	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		return http.csrf().disable()
-				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-				.and().build();
+	    return http.csrf().disable()
+	            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+	            .and().authorizeRequests()
+	            .antMatchers(HttpMethod.POST, "/login").permitAll()
+	            .antMatchers(HttpMethod.DELETE, "/medicos").hasRole("ADMIN")
+	            .antMatchers(HttpMethod.DELETE, "/pacientes").hasRole("ADMIN")
+	            .anyRequest().authenticated()
+	            .and().addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+	            .build();
 	}
 	
 	// @Bean Sem essa anotação no método, o objeto SecurityFilterChain não será exposto como um bean para o Spring.
--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import javax.management.RuntimeErrorException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,12 +15,16 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import med.voll.api.domain.usuario.UsuarioRepository;
 
 @Component // essa notação é utilizada quando queremos que o spring carregue uma classe generica
 public class SecurityFilter extends OncePerRequestFilter{
 	
 	@Autowired
 	private TokenService tokenService;
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -27,9 +34,19 @@ public class SecurityFilter extends OncePerRequestFilter{
 		
 		//Recuperar o token e passalo no cabeçalho da requisição
 		var tokenJWT = recuperarToken(request);
-
-		// Valida o token e recupera o subject
-		var subject = tokenService.getSubject(tokenJWT);
+		
+		if(tokenJWT != null) {
+			// Valida o token e recupera o subject
+			var subject = tokenService.getSubject(tokenJWT);
+			
+			// Trazendo o objeto usuario
+			var usuario = usuarioRepository.findByLogin(subject);
+			
+			
+			// Fazer o spring considerar o usuario autenticado
+			Authentication authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+			SecurityContextHolder.getContext().setAuthentication(authentication );
+		}
 		
 		// Necessário para chamar os próximos filtros na aplicação
 		filterChain.doFilter(request, response);
@@ -38,12 +55,14 @@ public class SecurityFilter extends OncePerRequestFilter{
 
 	private String recuperarToken(HttpServletRequest request) {
 		var authorizationHeader = request.getHeader("Authorization");
-		if(authorizationHeader == null) {
-			throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization!");
+		if(authorizationHeader != null) {
+			// Retira o prefixo padrão do Token JWT, o Bearer do token
+			return authorizationHeader.replace("Bearer", "");
 		}
 		
-		// Retira o prefixo padrão do Token JWT, o Bearer do token
-		return authorizationHeader.replace("Bearer", "");
+		return null;
+		
+		
 	}
 
 }
